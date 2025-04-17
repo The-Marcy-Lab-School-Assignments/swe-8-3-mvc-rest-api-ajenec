75dae5ebc8ad24031c5668893e3407d744144051
Ajene <ajenec@gmail.com>
2025-04-17 01:34:21 -0400

    Fixed it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import FoodDetails from "./pages/FoodDetails";
 
-function App() {}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/foods/:id" element={<FoodDetails />}></Route>
+    </Routes>
+  );
+}
 
 export default App;
